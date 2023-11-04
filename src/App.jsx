fc04958d6cf3c60cc73189045d8404b50c2cdf04
Álvaro Zájara <alzca1@gmail.com>
2023-11-04 20:56:37 +0100

    Sass added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logo from "/logo.jpeg";
+import { Login } from "./components/Login/Login.jsx";
+import Welcome from "./components/Welcome/Welcome.jsx";
+import { Button } from "antd";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mustShowLogin, setMustShowLogin] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <img src={logo} alt="job-tracker logo" />
+      <h1>Job Tracker</h1>
+      <h4>The best job-tracker around!</h4>
+      {mustShowLogin ? (
+        <Login />
+      ) : (
+        <div className="callToActionApp">
+          <Button
+            onClick={() => setMustShowLogin(!mustShowLogin)}
+            size="large"
+            shape="round"
+            type="primary"
+          >
+            Start tracking your jobs
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
