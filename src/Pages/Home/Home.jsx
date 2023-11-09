f77ad2f4f410e582bd2b5eee1073ec22b53a5d62
Álvaro Zájara <alzca1@gmail.com>
2023-11-09 14:30:36 +0100

    deleted AuthContext and changed jsx structure in Home component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,29 +13,16 @@ export default function Home() {
 
   return (
     <div className="Home">
-      {route === "/" ? (
-        <>
-          <img src={logo} alt="job-tracker logo" />
-          <h1>Job Tracker</h1>
-          <h4>The best job-tracker around!</h4>
-          <div className="callToActionApp">
-            <Link to="/login">
-              <Button size="large" shape="round" type="primary">
-                Start tracking your jobs
-              </Button>
-            </Link>
-          </div>
-        </>
-      ) : (
-        <div className="bar">
-          <Link to="/">
-            <div className="bar-logo">
-              <img src={logo} />
-              <span>Job Tracker</span>
-            </div>
-          </Link>
-        </div>
-      )}
+      <img src={logo} alt="job-tracker logo" />
+      <h1>Job Tracker</h1>
+      <h4>The best job-tracker around!</h4>
+      <div className="callToActionApp">
+        <Link to="/auth?mode=login">
+          <Button size="large" shape="round" type="primary">
+            Start tracking your jobs
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
