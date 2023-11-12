74e587c430ec3305c3b1c56abf52947c1e7dc555
Álvaro Zájara <alzca1@gmail.com>
2023-11-12 21:11:18 +0100

    deleted authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,12 @@ import ErrorPage from "./Pages/ErrorPage/ErrorPage.jsx";
 import SignUp from "./components/Auth/SignUp/SignUp.jsx";
 import Login from "./components/Auth/Login/Login.jsx";
 import UserHome from "./Pages/UserHome/UserHome.jsx";
-import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx";
-import { AuthProvider } from "./hooks/useAuth.jsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
