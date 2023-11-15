6c964d3b73dde9defaa5674af9337870a9e47b2e
Álvaro Zájara <alzca1@gmail.com>
2023-11-15 09:32:32 +0100

    added styling for Job component and organised prop data rendering

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export default function Job() {
-  return <div className="Job">Job</div>;
+export default function Job({ jobData }) {
+  const { companyName, position, dateApplied, status } = jobData;
+  return (
+    <div className="Job">
+      <div className="job-info">
+        <span>{position || "N/A"}</span>
+        <span>{companyName || "N/A"}</span>
+      </div>
+      <div className="job-status-date">
+        <span>{status || "N/A"}</span>
+        <span>{dateApplied || "N/A"}</span>
+      </div>
+    </div>
+  );
 }
