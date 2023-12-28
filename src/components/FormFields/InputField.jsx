e65e5e2243c7daec6fd0ac240562c43f59cf7445
Álvaro Zájara <alzca1@gmail.com>
2023-12-28 13:53:28 +0100

    added label to InputField props

--- src/components/FormFields/InputField.jsx
+++ src/components/FormFields/InputField.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { Form, Input } from "antd";
 
 export default function InputField(props) {
-  const { name, className, placeholder, defaultValue } = props;
+  const { name, className, placeholder, defaultValue, label } = props;
   return (
-    <Form.Item name={name}>
+    <Form.Item name={name} label={label}>
       <Input
         className={className}
         placeholder={placeholder}
