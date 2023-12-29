630c501418b22d8fb930a11ef246c2f75f0fd982
Álvaro Zájara <alzca1@gmail.com>
2023-12-29 10:48:36 +0100

    added logic to update application's requirements

--- src/components/Forms/JobRequirementsForm.jsx
+++ src/components/Forms/JobRequirementsForm.jsx
@@ -1,21 +1,51 @@
-import { Button, Form, Input, Select } from "antd";
-import { useEffect, useState } from "react";
+import { Button, Form, Select } from "antd";
+import { useState } from "react";
 import InputField from "../FormFields/InputField";
-import { DeleteOutlined } from "@ant-design/icons";
+import { DeleteOutlined, SaveOutlined } from "@ant-design/icons";
+import { useMutation } from "react-query";
+import { getToken } from "../../helpers/auth";
+import axios from "axios";
 
 const options = [
-  { value: "experience", label: "Experiencia mínima" },
-  { value: "education", label: "Estudios mínimos" },
-  { value: "residence", label: "Residencia" },
-  { value: "availability", label: "Disponibilidad" },
-  { value: "languages", label: "Idiomas" },
+  { value: "minimumExperience", label: "Experiencia mínima" },
+  { value: "educationRequired", label: "Estudios mínimos" },
+  { value: "residenceRequired", label: "Residencia" },
+  { value: "availabilityRequired", label: "Disponibilidad" },
+  { value: "languagesRequired", label: "Idiomas" },
 ];
-export default function JobRequirementsForm() {
+export default function JobRequirementsForm({ jobDetails: { _id } }) {
   const [requirements, setRequirements] = useState([]);
+  const [valuesAdded, setValuesAdded] = useState(false);
   const [form] = Form.useForm();
 
+  const headers = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getToken()?.token}`,
+    },
+  };
+
+  const mutation = useMutation({
+    mutationFn: (requirements) => {
+      const refinedRequirements = {
+        _id: _id,
+      };
+      for (let requirement of requirements) {
+        refinedRequirements[requirement.key] = requirement.value;
+      }
+
+      // hay que cocinar los requirements aquí
+      return axios.put(
+        `${import.meta.env.VITE_BACKEND_URL}/application/update`,
+        refinedRequirements,
+        headers
+      );
+    },
+  });
+
   const handleSubmit = (formValues) => {
-    console.log("i was clicked", formValues);
+    setValuesAdded(true);
+
     const requirement = {
       key: formValues.requirement.key,
       label: formValues.requirement.label,
@@ -26,27 +56,39 @@ export default function JobRequirementsForm() {
     console.log("requirement added", requirement);
   };
 
-  useEffect(() => {
-    console.log(requirements);
-  }, [requirements]);
+  // useEffect(() => {
+  //   console.log(requirements);
+  // }, [requirements]);
 
   const getFilteredOptions = () => {
     const filteredOptions = options.filter((option) => {
       return !requirements.some((requirement) => requirement.key === option.value);
     });
-    console.log(filteredOptions);
+    // console.log(filteredOptions);
     return filteredOptions;
   };
 
   const removeRequirement = (key) => {
     const filteredRequirements = [...requirements].filter((requirement) => requirement.key !== key);
     setRequirements(filteredRequirements);
+    if (filteredRequirements.length === 0) {
+      setValuesAdded(false);
+    }
+  };
+
+  const handleSaveRequirements = () => {
+    mutation.mutate(requirements);
   };
 
   return (
     <div>
+      <h3>Requirements</h3>
+      {valuesAdded ? (
+        <Button onClick={handleSaveRequirements}>
+          <SaveOutlined />
+        </Button>
+      ) : null}
       <div>
-        <h3>Requirements</h3>
         <ul>
           {requirements.map((requirement) => (
             <li key={requirement.key}>
